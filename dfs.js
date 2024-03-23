dbaabebbff1c816dca438eade0cfed1cefdd7a5d
abhayhk29 <abhayhk29@gmail.com>
2024-03-23 21:34:34 +0530

    dfx

---
+++ dfs.js
@@ -0,0 +1,25 @@
+// top to bottom
+//  example   + 
+            //  \\
+//          L    R
+
+// Pre Order => + -> L -> R
+// In Order => L -> + -> R
+// post order => l -> R -> +
+//             10
+//             /  \
+//            20   12
+//            /  \  
+//           15   13
+
+// Pre Order => Root => left -> Right
+//  10 -> 20 -> 15 -> 13 -> 12
+
+// INorder :  Left -> Root -> Right
+// 15-> 20 -> 13 -> 10 -> 10
+// lowest
+
+// post Order
+// 15 -> 13 -> 20 -> 12 -> 10
+// delete the tree
+
